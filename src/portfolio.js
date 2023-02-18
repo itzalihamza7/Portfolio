8ef0c95ac22e6714d662efb9de9ccddab92983f2
Ali Hamza <alihamzaali44@gmail.com>
2023-02-18 23:03:45 +0500

    app

---
+++ src/portfolio.js
@@ -0,0 +1,609 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Ali",
+  logo_name: "ali.h()",
+  nickname: "ali",
+  full_name: "Ali Hamza",
+  subTitle: "Full Stack Developer, Cyber Security Enthusiast 🔥. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/18QHdyWF_UfbAEY650wUjHBj0sAfinJPk/view?usp=share_link",
+  mail: "alihamzaali44@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/itzalihamza7",
+  linkedin: "https://www.linkedin.com/in/ali-hamza-613714177/",
+  gmail: "alihamzaali44@gmail.com",
+  gitlab: "https://gitlab.com/",
+  facebook: "https://www.facebook.com/alihamzaali44?mibextid=ZbWKwL",
+  twitter: "https://twitter.com/alihamzaftcr7?t=Ewgol7pvS27XgShzGr930A&s=09",
+  instagram: "https://instagram.com/itz_alihamza7?igshid=NTE5MzUyOTU=",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS and HTML/CSS",
+        "⚡ Developing mobile applications using React Native",
+        "⚡ Creating application backend in Node, Express & Rails",
+        "⚡ Integration of third party services such as Firebase",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        // {
+        //   skillName: "PHP",
+        //   fontAwesomeClassname: "simple-icons:php",
+        //   style: {
+        //     color: "#7377AD",
+        //   },
+        // },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        // {
+        //   skillName: "Android",
+        //   fontAwesomeClassname: "simple-icons:android",
+        //   style: {
+        //     color: "#3DDC84",
+        //   },
+        // },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "ROR",
+          fontAwesomeClassname: "simple-icons:rails",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Ruby",
+          fontAwesomeClassname: "simple-icons:ruby",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        // {
+        //   skillName: "Kali Linux",
+        //   fontAwesomeClassname: "simple-icons:kalilinux",
+        //   style: {
+        //     color: "#207297",
+        //   },
+        // },
+        {
+          skillName: "Postgress",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Github",
+          fontAwesomeClassname: "simple-icons:github",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cybersecurity",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working in Cryptography",
+        "⚡ Experience in penetration testing",
+        "⚡ Experience in Web Vulneribility checking",
+      ],
+      softwareSkills: [
+        {
+          skillName: "KALI Linux",
+          fontAwesomeClassname: "simple-icons:kalilinux",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Wireshark",
+          fontAwesomeClassname: "simple-icons:wireshark",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Tor",
+          fontAwesomeClassname: "simple-icons:tor",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "SQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "Etherium",
+          fontAwesomeClassname: "simple-icons:ethereum",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "National University of Sciences and technology(NUST)",
+      subtitle: "Bachelor in Computer Science",
+      logo_path: "nust.png",
+      alt_name: "BSCS",
+      duration: "2018 - 2022",
+      descriptions: [
+        "⚡ I studied core computer science subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I completed various online courses for Backend , Web , Mobile App Development, etc.",
+        "⚡ Always keen to do the practical work and showed great interest in all the semester projects.",
+        "⚡ Took part in many events, societies, co-curricular activities and  Won many tournaments being captain of the Football Team at NUST",
+       ],
+      website_link: "https://nust.edu.pk/",
+    },
+    {
+      title: "Punjab Group of Colleges",
+      subtitle: "FSC (Pre Eng)",
+      logo_path: "pgc.png",
+      alt_name: "FSC",
+      duration: "2016 - 2018",
+      descriptions: [
+         "⚡ Studied the basics of Engineering Subjects and developed my interest in computer science ",
+        "⚡ Lead my college as athlete and was alos active in other co-curricular activities ",
+      ],
+      website_link: "https://pgc.edu/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Hackathon",
+      subtitle: "ACM Hackathon",
+      logo_path: "hackathon1.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/1HyR1A6lgQjvDeiIv0_LxolX_kzvFE_Q2/view?usp=share_link",
+      alt_name: "hackathon",
+      color_code: "#E2405F",
+    },
+    {
+      title: "Cyber Security & Cyber Forensics",
+      subtitle: "Workshop at NUST",
+      logo_path: "iit.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12auzjUKxrxPJFN9ZowaNzSYqNmO3y6Pt/view?usp=share_link",
+      alt_name: "Workshop",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "MLH Local Hack Day: Build",
+      subtitle: "Major League Hacking",
+      logo_path: "mlh-logo.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/1HyR1A6lgQjvDeiIv0_LxolX_kzvFE_Q2/view?usp=share_link",
+      alt_name: "Google",
+      color_code: "#fe0037",
+    },
+    {
+      title: "Hack20",
+      subtitle: "Flutter International Hackathon",
+      logo_path: "flutter.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1G_6Gz0IEgX5OGxoHZJruxVJXUEx6Htr_/view?usp=share_link",
+      alt_name: "Flutter International Hackathon",
+      color_code: "#2AAFED",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I have done Internships and jobs in feild of computer science and have experience of an year. I have been a active freelancer and developed many projects for clients. I am looking forward to use my skills in this feild.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Associate Software Developer",
+          company: "Devsinc",
+          company_url: "https://devsinc.com/",
+          logo_path: "devsinc.jpeg",
+          duration: "Jul 2022 - Jan 2023",
+          location: "Islamabad, Pakistan",
+          description: `
+          • Developed scalable Full stack solutions using ROR, MERN Stack and ReactJs.
+          • Helped the company to scale market-ready solutions for US-based clients.
+          • Contributed on production level code with Shopify and Big Commerce.
+          • Worked on technologies like React Js and Bootstrap for frontend.
+          • Worked Collaboratively with QAs to complete comprehensive testing on major software release.
+          • Consistently recognized for excellent problem solving and analytical skills by stakeholders.
+          
+          `,
+            color: "#0071C5",
+        },
+        {
+          title: "Networks Engineer (Intern)",
+          company: "PTCL",
+          company_url: "https://ptcl.com.pk/",
+          logo_path: "ptcl.png",
+          duration: "JUN 2021 - AUG 2021",
+          location: "Islamabad, Pakistan",
+          description: `• Worked under network security administrator monitoring company's network 
+          • Developed an excel based network maintenance application.     `,
+              color: "#0071C5",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+       
+        {
+          title: "GDG Student Volunteer",
+          company: "Google Developer Groups",
+          company_url: "https://gdg.community.dev/",
+          logo_path: "gdg.png",
+          duration: "Feb 2021 - Dec 2021",
+          location: "Work From Home",
+          description:
+            "Google Developer Group Surat Student Volunteer and Member.",
+          color: "#D83B01",
+        },
+        {
+          title: "GitHub Student Developer",
+          company: "GitHub",
+          company_url: "https://github.com/",
+          logo_path: "github.png",
+          duration: "Nov 2019 - JUL 2020",
+          location: "Work from Home",
+          description:
+            "Contribute to Open Source Community and Open Source Project.",
+          color: "#040f26",
+        },
+        {
+          title: "Google Local Guide",
+          company: "Google Map",
+          company_url: "https://maps.google.com/localguides/",
+          logo_path: "localguide.png",
+          duration: "Sep 2019 - JUN 2020",
+          location: "Work From Home",
+          description:
+            "Day-to-day responsibilities of helping local businesses to spread their business to the world. Helping users by writing reviews about different locations and spaces such as shops, malls, etc.",
+          color: "#D83B01",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS/Rails Backend Projects, Python Scripts, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "ali.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://twitter.com/alihamzaftcr7?t=Ewgol7pvS27XgShzGr930A&s=09",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Al-Tabeeb",
+      url: "https://github.com/itzalihamza7/Al-Tabeeb",
+      description:
+        "A Decentralized online hospital management system where patients can securely get appointment from doctor and get the prescription after checkup.",
+      languages: [
+        {
+          name: "Angularjs",
+          iconifyClass: "logos-angular",
+        },
+        {
+          name: "Django",
+          iconifyClass: "logos-django",
+        },
+        {
+          name: "Etherium",
+          iconifyClass: "logos-ethereum",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "UI Radiance",
+      url: "https://github.com/itzalihamza7/UIRadiance",
+      description:
+        "An app to test the UI skills",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      id: "13",
+      name: "BlogApp",
+      url: "https://github.com/itzalihamza7/BlogApp",
+      description:
+        "A website where we can share blogs, edit them, comment on them , like and many more features",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+        {
+          name: "Rails",
+          iconifyClass: "logos-rails",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Ecommerece Store",
+      url: "https://github.com/itzalihamza7/Ecommerece",
+      description:
+        "An Ecommerece store app to sell and buy products",
+      languages: [
+        {
+          name: "Rails",
+          iconifyClass: "logos-rails",
+        },
+        {
+          name: "ReactJs",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Stripe",
+          iconifyClass: "logos-stripe",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "Ewallet",
+      url: "https://github.com/itzalihamza7/Ewallet",
+      description:
+        "An online wallet to where we can send and receive money from others",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "Rails",
+          iconifyClass: "logos-rails",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "personal-portfolio",
+      url: "https://github.com/itzalihamza7/Portfolio",
+      description:
+        "An app to show my portfolio",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "BurgerApp",
+      url: "https://github.com/itzalihamza7/BurgerApp",
+      description: "App to test reactjs skills",
+      languages: [
+        {
+          name: "react",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
